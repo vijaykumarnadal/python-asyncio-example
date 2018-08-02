b7c5043209d62b0a1a212362f37aecbb240d8514
vijaykumarnadal <33896298+vijaykumarnadal@users.noreply.github.com>
2018-08-02 12:15:34 +0530

    Update and rename Method_asyncio.py to method_asyncio.py

--- method_asyncio.py
+++ method_asyncio.py
@@ -12,16 +12,16 @@ context_intent_text_list3=[['hotel','checkin','checking in'],['hotel','lorem','i
 class Asyncio(object):
         def __init__(self):
             pass
-        async def Data_extraction(self,textdata):
+        async def data_extraction(self,textdata):
                 my_list1,my_list2,my_list3=[x[0] for x in textdata],[x[1]for x in textdata],[x[2] for x in textdata]
                 #await asyncio.sleep(0.0001)
                 df2=pd.DataFrame({'context':my_list1,'intent':my_list2,'data':my_list3,'timestamp':datetime.now()})
                 await asyncio.sleep(0.0001)
                 print(df2)
-        async def Method_asyncio(self):
-                ext1 = loop.create_task(self.Data_extraction(context_intent_text_list))
-                ext2 = loop.create_task(self.Data_extraction(context_intent_text_list2))
-                ext3 = loop.create_task(self.Data_extraction(context_intent_text_list3))
+        async def method_asyncio(self):
+                ext1 = loop.create_task(self.data_extraction(context_intent_text_list))
+                ext2 = loop.create_task(self.data_extraction(context_intent_text_list2))
+                ext3 = loop.create_task(self.data_extraction(context_intent_text_list3))
                 await asyncio.wait([ext1,ext2,ext3])
     
 
@@ -29,5 +29,5 @@ if __name__ == '__main__':
      de=Asyncio()
      loop = asyncio.new_event_loop()
      asyncio.set_event_loop(loop)
-     loop.run_until_complete(de.Method_asyncio())
+     loop.run_until_complete(de.method_asyncio())
      loop.close()
